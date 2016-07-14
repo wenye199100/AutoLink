6af7cfe88ab21fd0f4788775ca02a41edec48888
wenye199100 <two_ye@hotmail.com>
2016-07-15 00:00:21 +0800

    将分词和数据库连接分离成2块

--- Segmentation.py
+++ Segmentation.py
@@ -1,30 +1,29 @@
 #encoding: utf-8
 import jieba
-import DBConnetion
+import jieba.posseg as pseg
+import re
+
+def cut(string):
+    seg_list = jieba.cut(string, cut_all=False)
+    print(','.join(seg_list))
+
+def poosseg(string):
+    words = pseg.cut(string)
+    for word, flag in words:
+        pattern = re.compile(r'^nw?')
+        pattern2 = re.compile(r'eng')
+        match = pattern.match(flag)
+        match2 = pattern2.match(flag)
+        if match or match2:
+            print('%s %s' % (word, flag))
+
 
 if __name__ == "__main__":
 
-    file = open('Data/file.txt')
-    B = DBConnetion.connection('oil-companies','select company_id,name,website from company_data')
-    dict_name_id = {}
-    dict_id_web = {}
-    for i in B:
-        if i[1] and i[2]:
-            dict_name_id[i[1]] = i[0]
-            dict_id_web[i[0]] = i[2]
-            print i[0], i[1], i[2]
-    dict_id_web = sorted(dict_id_web.iteritems(), key=lambda d: d[0], reverse=False)
-    print dict_id_web
-       #print i[0].encode('gbk')
-       # output2.write(i[0] + "\n")
-    #file.close()
-    #output.close()
+    #file = open('Data/file.txt')
+    string = "在如今越来越追求效率的作业环境下，整个行业都在想方设法提高操作安全的同时不断降低总体作业成本。DynaStage是DynaEnergetics公司推出的关于射孔的一个全新概念，它开创性的将编址技术和选择性技术同改良的机械设计进行整合，可以完全消除潜在的人为错误。该系统比传统的射孔枪作业效率更高，由于搭载额外的安全设计，在射孔期间还允许同时进行其他作业，这种设计可以提高射孔的质量和作业性能可靠性，减少入井失败的次数并降低库存量。DynaStage系统已经成功地在多个盆地完成了作业，该新系统针对三个主要方面来提高作业效率和降低作业成本：安全、可靠性、库存和成本控制。"
+    #seg_list = jieba.cut_for_search(string)  # 搜索引擎模式
+    cut(string)
 
-    # for line in file:
-    #     print("1" + line.decode("gbk"))
-    # string = "在如今越来越追求效率的作业环境下，整个行业都在想方设法提高操作安全的同时不断降低总体作业成本。DynaStage是DynaEnergetics公司推出的关于射孔的一个全新概念，它开创性的将编址技术和选择性技术同改良的机械设计进行整合，可以完全消除潜在的人为错误。该系统比传统的射孔枪作业效率更高，由于搭载额外的安全设计，在射孔期间还允许同时进行其他作业，这种设计可以提高射孔的质量和作业性能可靠性，减少入井失败的次数并降低库存量。DynaStage系统已经成功地在多个盆地完成了作业，该新系统针对三个主要方面来提高作业效率和降低作业成本：安全、可靠性、库存和成本控制。"
-    #
-    # seg_list = jieba.cut(string, cut_all=False)
-    #
-    # print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
+
 
